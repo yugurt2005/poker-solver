8ea1e1aa371742760182acc66efbbfca327bf24c
Chien-Yu Xiong <58307327+yugurt2005@users.noreply.github.com>
2024-08-20 00:36:22 -0700

    logging

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use poker_solver::{poker::Poker, solver::solve};
 fn main() {
     let game = Poker::new("data/abstraction/".to_string());
 
-    let data = solve(10000000, 42, &game);
+    let start = std::time::Instant::now();
+
+    let data = solve(1000000000, 42, &game);
+
+    println!("Elapsed: {:?}", start.elapsed());
 
     let buffer = bincode::serialize(&data).unwrap();
     std::fs::File::create("data/solution.bin".to_string())
